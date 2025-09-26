31f5ea334d0a0f930441a2ce8df9f3a030d4502f
Daniel Zeegler <155176365+zeeglerPrograms@users.noreply.github.com>
2025-09-26 12:07:22 -0500

    added users

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const blogRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const app = express()
 
@@ -23,6 +25,8 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 
 app.use(middleware.unknownEndpoint)
